dca10c7390bef5a786adff3f0c28f4b5bc66008e
ChapST1 <cutitox59@gmail.com>
2024-03-09 01:24:16 -0500

    add icons & project images

--- public/data/site-config.js
+++ public/data/site-config.js
@@ -80,68 +80,129 @@ export const siteConfig = {
 			  se han adquirido a través de mi formación académica,
 			 	experiencia en prácticas y aprendizaje autodidacta.
 			`,
-			data: [
-				{
-					name: icons.html.name,
-					icon: icons.html.icon,
+			data: {
+				languages: {
+					title: "Lenguajes",
+					data: [
+						{
+							name: icons.html.name,
+							icon: icons.html.icon,
+							isLearning: false,
+						},
+						{
+							name: icons.css.name,
+							icon: icons.css.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.javascript.name,
+							icon: icons.javascript.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.typeScript.name,
+							icon: icons.typeScript.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.sass.name,
+							icon: icons.sass.icon,
+							isLearning: false,
+						},
+					],
 				},
-				{
-					name: icons.css.name,
-					icon: icons.css.icon,
+				frameworks: {
+					title: "Frameworks y Librerías",
+					data: [
+						{
+							name: icons.next.name,
+							icon: icons.next.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.react.name,
+							icon: icons.react.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.vue.name,
+							icon: icons.vue.icon,
+							isLearning: true,
+						},
+						{
+							name: icons.tailwind.name,
+							icon: icons.tailwind.icon,
+							isLearning: false,
+						},
+
+						{
+							name: icons.astro.name,
+							icon: icons.astro.icon,
+							isLearning: false,
+						},
+					],
 				},
-				{
-					name: icons.javascript.name,
-					icon: icons.javascript.icon,
+				backendStack: {
+					title: "Backend Stack",
+					data: [
+						{
+							name: icons.node.name,
+							icon: icons.node.icon,
+							isLearning: true,
+						},
+						{
+							name: icons.mySql.name,
+							icon: icons.mySql.icon,
+							isLearning: false,
+						},
+						{
+							name: icons.sql.name,
+							icon: icons.sql.icon,
+							isLearning: false,
+						},
+					],
 				},
-				{
-					name: icons.typeScript.name,
-					icon: icons.typeScript.icon,
+				javascriptLibraries: {
+					title: "JavaScript - Librerías",
+					data: [
+						{
+							name: icons.pinia.name,
+							icon: icons.pinia.icon,
+							isLearning: true,
+						},
+						{
+							name: icons.zustand.name,
+							icon: icons.zustand.icon,
+							isLearning: false,
+						},
+						{
+							name: icons["react-router-dom"].name,
+							icon: icons["react-router-dom"].icon,
+							isLearning: false,
+						},
+					],
 				},
-				{
-					name: icons.sass.name,
-					icon: icons.sass.icon,
+				tools: {
+					title: "Herramientas",
+					data: [
+						{
+							name: icons.git.name,
+							icon: icons.git.icon,
+							isLearning: false,
+						},
+						{
+							name: icons.github.name,
+							icon: icons.github.icon,
+							isLearning: false,
+						},
+					],
 				},
-				{
-					name: icons.tailwind.name,
-					icon: icons.tailwind.icon,
-				},
-				{
-					name: icons.react.name,
-					icon: icons.react.icon,
-				},
-				{
-					name: icons.next.name,
-					icon: icons.next.icon,
-				},
-				{
-					name: icons.astro.name,
-					icon: icons.astro.icon,
-				},
-				{
-					name: icons.node.name,
-					icon: icons.node.icon,
-				},
-				{
-					name: icons.mySql.name,
-					icon: icons.mySql.icon,
-				},
-				{
-					name: icons.sql.name,
-					icon: icons.sql.icon,
-				},
-				{
-					name: icons.git.name,
-					icon: icons.git.icon,
-				},
-				{
-					name: icons.github.name,
-					icon: icons.github.icon,
-				},
-				{
-					name: icons.zustand.name,
-					icon: icons.zustand.icon,
-				},
-			],
+			},
 		},
 		projects: {
 			title: "Mis Proyectos",
@@ -149,24 +210,57 @@ export const siteConfig = {
 			 En esta sección, se describen los proyectos personales y
 			 profesionales en los que he participado. Estos proyectos demuestran 
 			 mis habilidades y conocimientos como desarrollador web, así como mi capacidad para 
-			 trabajar de forma independiente y en equipo
+			 trabajar de forma independiente y en equipo.
 			`,
 			data: [
 				{
-					name: "Gallery",
-					image: "./public/projects/1.webp",
-					info: "Una plataforma para descargar wallpapers ",
+					name: "Wallpapers -  Web",
+					image: "/projects/1.webp",
+					info: "Una plataforma para descargar wallpapers.",
 					demo: "https://gruvbox-wallpapers.vercel.app/",
 					repo: "https://github.com/ChapST1/gruvbox-wallpapers-web",
 					stack: ["Astro", "TailwindCss", "JavaScript", "Node JS"],
+					completed: true,
 				},
 				{
 					name: "Caroni Computer",
-					image: "./public/projects/2.webp",
-					info: "Un cambio de estilos al comercio electronico",
+					image: "/projects/2.webp",
+					info: "Un cambio de estilos al comercio electronico.",
 					demo: "https://caronicomputer.com",
 					repo: null,
 					stack: ["Html", "Css", "JavaScript"],
+					completed: true,
+				},
+				{
+					name: "Wsp bot",
+					image: "/projects/3.webp",
+					info: "Una forma fácil de crear bots de whatsapp.",
+					demo: "https://wsp-bot-ease-chapst1.vercel.app/chatbot",
+					repo: "https://github.com/ChapST1/wsp-bot-easy",
+					stack: [
+						"React Js",
+						"Zustand JS",
+						"TypeScript",
+						"Framer motion",
+						"React router dom",
+					],
+					completed: false,
+				},
+				{
+					name: "Movie DBX",
+					image: "/projects/4.webp",
+					info: "Un clon de (themoviedb).",
+					demo: "https://movie-dbx.vercel.app/",
+					repo: "https://github.com/ChapST1/movies-db-clonex",
+					stack: [
+						"Next JS",
+						"Tailwind css",
+						"React Js",
+						"ShadcnUI",
+						"Zustand JS",
+						"TypeScript",
+					],
+					completed: false,
 				},
 			],
 		},
